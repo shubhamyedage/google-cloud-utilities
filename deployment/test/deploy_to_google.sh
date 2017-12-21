61c27ded21ebecf05618bfbe43cc559193d4d833
shubham.yedage <shubham.yedage@synerzip.com>
2017-12-21 18:39:43 +0530

    Create instance testmachine

--- deployment/test/deploy_to_google.sh
+++ deployment/test/deploy_to_google.sh
@@ -9,6 +9,4 @@ codeship_google authenticate
 
 echo "Starting a small cluster with a single instance"
 gcloud config set compute/zone us-central1-a
-gcloud container clusters create "${cluster_name}" \
-  --num-nodes 1 \
-  --machine-type g1-small
+gcloud compute instances create testmachine
