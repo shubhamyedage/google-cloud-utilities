1ae58b0ed70d1b56eb4cb7291313b53738942a53
shubham.yedage <shubham.yedage@synerzip.com>
2017-12-21 15:39:48 +0530

    Test set 2

--- deployment/test/deploy_to_google.sh
+++ deployment/test/deploy_to_google.sh
@@ -4,6 +4,7 @@ set -e
 date=$(date "+%Y%m%d%H%M")
 cluster_name="codeship-gcloud-test-${date}"
 echo "Authenticating"
+ls
 codeship_google authenticate
 
 echo "Starting a small cluster with a single instance"
