45f1c5e9169b5e471af5ee83394c1cd90dd35550
shubham.yedage <shubham.yedage@synerzip.com>
2017-12-22 14:20:40 +0530

    Update dir name deployment to deploy

---
+++ deploy/test/dummy_deploy_to_google.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e
+
+date=$(date "+%Y%m%d%H%M")
+cluster_name="codeship-gcloud-test-${date}"
+
+codeship_google authenticate
+
+echo "Starting a small cluster with a single instance"
+gcloud config set compute/zone us-central1-a
+gcloud container clusters create "${cluster_name}" \
+  --num-nodes 1 \
+  --machine-type g1-small
+
+echo "Shutting everything down again"
+gcloud container clusters delete "${cluster_name}" -q
